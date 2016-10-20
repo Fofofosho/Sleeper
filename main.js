76d32d55f0b7cf339134812cf7812089732b017f
Forrest <forrest.town18@gmail.com>
2016-10-19 20:52:23 -0700

    Replaced Electron icon with a lame S.

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
+const path = require('path');
 
 let mainWindow;
 
@@ -10,7 +11,10 @@ app.on("ready", function() {
             width: 800,
             height: 600,
             title: "Sleeper",
-            autoHideMenuBar: true
+            autoHideMenuBar: true,
+            //only impacts Windows/Linux
+            //http://stackoverflow.com/questions/31529772/setting-app-icon-for-electron-atom-shell-app
+            icon: path.join(__dirname, "icon.png")
         });
 
     mainWindow.loadURL(`file://${__dirname}/index.html`);
